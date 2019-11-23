aae5c806ee587107cb3ac2aa8378706ba8da13b4
MasuqaT <occar@hotmail.co.jp>
2019-11-23 21:20:33 +0900

    refactor: split out files for hands

---
+++ engine-core/src/variations/jp4s17t/hands/all_in_triplets.rs
@@ -0,0 +1,113 @@
+use crate::hands::{Hand, HandTestResult};
+use super::super::tile::Tile;
+use itertools::Itertools;
+use super::super::game::{PlayerHandJp4s17t, WinningPoint};
+use std::collections::HashMap;
+use std::marker::PhantomData;
+use crate::game::Meld;
+use super::FanHand;
+
+/// 対々和
+struct AllInTriplets;
+
+impl FanHand<AllInTriplets> {
+    pub(crate) fn new(closed_han: u8, open_han: u8) -> FanHand<AllInTriplets> {
+        FanHand {
+            closed_han,
+            open_han,
+            phantom: PhantomData,
+        }
+    }
+
+    fn test(&self, player_hand: &PlayerHandJp4s17t, new_tile: &Tile) -> HandTestResult<WinningPoint> {
+        if !player_hand.melds.iter().all(|m| match m {
+            Meld::Kong(_, _) | Meld::Pong(_, _) => true,
+            _ => false
+        }) {
+            return HandTestResult::Nothing;
+        }
+
+        let winning_result = HandTestResult::Winning(WinningPoint::Fan(if player_hand.is_closed() { self.closed_han } else { self.open_han }));
+
+        let mut tiles = player_hand.closed_tiles.clone();
+        tiles.insert(*new_tile);
+        let n_closed_tiles = tiles.len();
+        let groups = tiles.get_by_buckets();
+        let groups = groups.group_by(|&(_, &n)| n);
+        let map: HashMap<_, Vec<_>> = groups.into_iter().map(|(n, gv)| (n, gv.collect())).collect();
+
+        if map.keys().len() == 1 && map.contains_key(&2) {
+            return winning_result;
+        }
+        match (map.get(&2), map.get(&3)) {
+            (Some(n2), Some(n3)) if n2.len() == 1 && n3.len() == (n_closed_tiles - 2) / 3 =>
+                winning_result,
+            _ => HandTestResult::Nothing
+        }
+    }
+}
+
+impl Hand<PlayerHandJp4s17t> for FanHand<AllInTriplets> {
+    type Point = WinningPoint;
+    type Tile = Tile;
+
+    fn test_completion_on_drawing(&self, player_hand: &PlayerHandJp4s17t, drawn_tile: &Self::Tile) -> HandTestResult<Self::Point> {
+        self.test(player_hand, drawn_tile)
+    }
+
+    fn test_completion_when_discarded(&self, player_hand: &PlayerHandJp4s17t, discarded_tile: &Self::Tile) -> HandTestResult<Self::Point> {
+        self.test(player_hand, discarded_tile)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::AllInTriplets;
+    use crate::hands::{Hand, HandTestResult};
+    use super::super::super::game::{PlayerHandJp4s17t, WinningPoint};
+    use super::super::super::tile::Tile::{Number, Wind, Symbol};
+    use super::super::super::tile::Suite::{Green, Red, White, Black};
+    use super::super::FanHand;
+    use crate::game::{Meld, Side};
+
+    #[test]
+    /// 裸単騎待ち
+    fn when_drawn_wins_only_one_tile() {
+        let matcher = FanHand::<AllInTriplets>::new(2, 2);
+        let hand = PlayerHandJp4s17t::create(
+            vec![Number(Green, 6)],
+            (1..=5).map(|i| Number(Green, i))
+                .map(|t| Meld::Pong([t, t, t], Side::Left))
+                .collect(),
+            vec![],
+        );
+        let result = matcher.test_completion_on_drawing(&hand, &Number(Green, 6));
+        assert_eq!(result, HandTestResult::Winning(WinningPoint::Fan(2)));
+    }
+
+    #[test]
+    fn when_drawn_wins_with_double_wait() {
+        let matcher = FanHand::<AllInTriplets>::new(2, 2);
+        let hand = PlayerHandJp4s17t::create(
+            vec![Number(Green, 5), Number(Green, 5), Number(Green, 6), Number(Green, 6)],
+            (1..=4).map(|i| Number(Green, i))
+                .map(|t| Meld::Pong([t, t, t], Side::Left))
+                .collect(),
+            vec![],
+        );
+        let result = matcher.test_completion_on_drawing(&hand, &Number(Green, 6));
+        assert_eq!(result, HandTestResult::Winning(WinningPoint::Fan(2)));
+    }
+
+    #[test]
+    fn when_drawn_nothing_happens() {
+        let matcher = FanHand::<AllInTriplets>::new(2, 2);
+        let hand = PlayerHandJp4s17t::create(
+            (1..=8).map(|i| Number(Red, i))
+                .map(|t| vec![t, t])
+                .flatten(),
+            vec![], vec![]);
+        let result = matcher.test_completion_on_drawing(&hand, &Number(Red, 9));
+        assert_eq!(result, HandTestResult::Nothing);
+    }
+}
---
+++ engine-core/src/variations/jp4s17t/hands/eight_pairs_and_half.rs
@@ -0,0 +1,80 @@
+use crate::hands::{Hand, HandTestResult};
+use super::super::tile::Tile;
+use itertools::Itertools;
+use super::super::game::{PlayerHandJp4s17t, WinningPoint};
+use std::collections::HashMap;
+use std::marker::PhantomData;
+use super::FanHand;
+
+/// 八対子半
+struct EightPairsAndHalf;
+
+impl FanHand<EightPairsAndHalf> {
+    pub(crate) fn new(closed_han: u8, open_han: u8) -> FanHand<EightPairsAndHalf> {
+        FanHand {
+            closed_han,
+            open_han,
+            phantom: PhantomData,
+        }
+    }
+
+    fn test(&self, player_hand: &PlayerHandJp4s17t, new_tile: &Tile) -> HandTestResult<WinningPoint> {
+        let mut tiles = player_hand.closed_tiles.clone();
+        tiles.insert(*new_tile);
+        let groups = tiles.get_by_buckets();
+        let groups = groups.group_by(|&(_, &n)| n);
+        let map: HashMap<_, Vec<_>> = groups.into_iter().map(|(n, gv)| (n, gv.collect())).collect();
+        match (map.get(&2), map.get(&3)) {
+            (Some(n2), Some(n3)) if n2.len() == 7 && n3.len() == 1 =>
+                HandTestResult::Winning(WinningPoint::Fan(if player_hand.is_closed() { self.closed_han } else { self.open_han })),
+            _ => HandTestResult::Nothing
+        }
+    }
+}
+
+impl Hand<PlayerHandJp4s17t> for FanHand<EightPairsAndHalf> {
+    type Point = WinningPoint;
+    type Tile = Tile;
+
+    fn test_completion_on_drawing(&self, player_hand: &PlayerHandJp4s17t, drawn_tile: &Self::Tile) -> HandTestResult<Self::Point> {
+        self.test(player_hand, drawn_tile)
+    }
+
+    fn test_completion_when_discarded(&self, player_hand: &PlayerHandJp4s17t, discarded_tile: &Self::Tile) -> HandTestResult<Self::Point> {
+        self.test(player_hand, discarded_tile)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::EightPairsAndHalf;
+    use crate::hands::{Hand, HandTestResult};
+    use super::super::super::game::{PlayerHandJp4s17t, WinningPoint};
+    use super::super::super::tile::Tile::{Number, Wind, Symbol};
+    use super::super::super::tile::Suite::{Green, Red, White, Black};
+    use super::FanHand;
+
+    #[test]
+    fn when_drawn_wins() {
+        let matcher = FanHand::<EightPairsAndHalf>::new(2, 1);
+        let hand = PlayerHandJp4s17t::create(
+            (1..=8).map(|i| Number(Green, i))
+                .map(|t| vec![t, t])
+                .flatten(),
+            vec![], vec![]);
+        let result = matcher.test_completion_on_drawing(&hand, &Number(Green, 1));
+        assert_eq!(result, HandTestResult::Winning(WinningPoint::Fan(2)));
+    }
+
+    #[test]
+    fn when_drawn_nothing_happens() {
+        let matcher = FanHand::<EightPairsAndHalf>::new(2, 1);
+        let hand = PlayerHandJp4s17t::create(
+            (1..=8).map(|i| Number(Red, i))
+                .map(|t| vec![t, t])
+                .flatten(),
+            vec![], vec![]);
+        let result = matcher.test_completion_on_drawing(&hand, &Number(Red, 9));
+        assert_eq!(result, HandTestResult::Nothing);
+    }
+}
