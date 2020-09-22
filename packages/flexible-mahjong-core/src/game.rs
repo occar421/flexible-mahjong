11f61cf9d57ec500d5bb52edbba270135e372bfb
MasuqaT <occar@hotmail.co.jp>
2020-09-22 20:01:12 +0900

    WIP: circular ref stabilized

--- packages/flexible-mahjong-core/src/game.rs
+++ packages/flexible-mahjong-core/src/game.rs
@@ -1,4 +1,5 @@
 use std::rc::{Rc, Weak};
+use std::cell::RefCell;
 
 pub trait Concept {
     type Tile;
@@ -7,6 +8,8 @@ pub trait Concept {
 }
 
 pub trait TileDealingSpec<C: Concept> {}
+
+struct TableFacade<C: Concept>(Rc<RefCell<Table<C>>>);
 
 struct Table<C: Concept> {
     tile_dealing_spec: Box<dyn TileDealingSpec<C>>,
@@ -17,26 +20,27 @@ struct Table<C: Concept> {
     players: Option<[(Player<C>, Seat); 4]>,
 }
 
-impl<C: Concept> Table<C> {
-    pub fn new(tile_dealing_spec: Box<dyn TileDealingSpec<C>>) -> Table<C> {
-        Table {
-            tile_dealing_spec,
-            wall_tiles: vec![],
-            supplemental_tiles: vec![],
-            reward_indication_tiles: vec![],
-            progress: Progress::get_initial(),
-            players: None,
-        }
+impl<C: Concept> TableFacade<C> {
+    pub fn new(tile_dealing_spec: Box<dyn TileDealingSpec<C>>) -> TableFacade<C> {
+        TableFacade(Rc::new(RefCell::new(
+            Table {
+                tile_dealing_spec,
+                wall_tiles: vec![],
+                supplemental_tiles: vec![],
+                reward_indication_tiles: vec![],
+                progress: Progress::get_initial(),
+                players: None,
+            })))
     }
 
     fn map_player(&self, player: (Box<dyn ActionPolicy<C>>, Seat)) -> (Player<C>, Seat) {
-        let self_ref = Rc::downgrade(Rc::new(self));
+        let self_ref = Rc::downgrade(&self.0.clone());
         (Player::new(self_ref, player.0), player.1)
     }
 
     pub fn join_users(&mut self, players: [(Box<dyn ActionPolicy<C>>, Seat); 4]) {
         let [player0, player1, player2, player3] = players;
-        self.players = Some([
+        self.0.borrow_mut().players = Some([
             self.map_player(player0),
             self.map_player(player1),
             self.map_player(player2),
@@ -84,11 +88,11 @@ struct Player<C: Concept> {
     concealed_tiles: Vec<C::Tile>,
     exposed_melds: Vec<C::Meld>,
     discarded_tiles: Vec<C::Tile>,
-    table: Weak<Table<C>>,
+    table: Weak<RefCell<Table<C>>>,
 }
 
 impl<C: Concept> Player<C> {
-    fn new<'a>(table: Weak<Table<C>>, action_policy: Box<dyn ActionPolicy<C>>) -> Player<C> {
+    fn new<'a>(table: Weak<RefCell<Table<C>>>, action_policy: Box<dyn ActionPolicy<C>>) -> Player<C> {
         Player {
             point: 0,
             action_policy,
